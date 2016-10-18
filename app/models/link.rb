0eca0c20badb4460ab3344b3bafd4671089147ff
david anderson <d.a.m.anderson@gmail.com>
2016-10-18 11:45:42 +0100

    Added development database

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,7 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+# require './app/app'
+
 
 class Link
 
@@ -11,6 +13,6 @@ class Link
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test")
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
