61ea3a280504bf28b5c78034973e8efc5e30ef94
david anderson <d.a.m.anderson@gmail.com>
2016-10-20 15:18:14 +0100

    password verification

--- spec/user_management_spec.rb
+++ spec/user_management_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
 feature 'User sign up' do
+
   scenario 'I can sign in as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq ('alice@example.com')
   end
+
+  scenario 'requires a matching password confirmation' do
+    expect {sign_up(password_confirmation: 'wrong')}.not_to change(User, :count)
+  end
+
 end
