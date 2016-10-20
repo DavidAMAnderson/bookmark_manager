706bae1d8e0c6321948366a74e9c0f3fa31d700a
david anderson <d.a.m.anderson@gmail.com>
2016-10-20 14:59:36 +0100

    adding user accounts

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,16 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
-
 require_relative 'datamapper_setup'
 
 class BookmarkManager < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  get '/' do
+    redirect to('/links')
+  end
 
   get '/links' do
     @links = Link.all
@@ -18,7 +24,7 @@ class BookmarkManager < Sinatra::Base
   post '/links' do
     link = Link.create(url: params[:url],title: params[:title])
     params[:tags].split.each do |tag|
-      link.tags << Tag.create(name: tag)
+      link.tags << Tag.first_or_create(name: tag)
     end
     link.save
     redirect to('/links')
@@ -28,6 +34,22 @@ class BookmarkManager < Sinatra::Base
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
     erb :'links/index'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/links')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
   end
 
 
